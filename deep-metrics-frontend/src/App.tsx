ed06a3df84ec64428accb8ea2475a959aac783bb
justynaprzyborska <justyna.przyborska123@gmail.com>
2025-03-11 21:05:46 +0000

    fixed nav bug

--- deep-metrics-frontend/src/App.tsx
+++ deep-metrics-frontend/src/App.tsx
@@ -1,30 +1,19 @@
 import React from "react";
 import styled from "styled-components";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useLocation, matchPath } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MetricPage from "./pages/MetricPage";
-import { useLocation, matchPath } from "react-router-dom";
-
-
 
 const Header = styled.header`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  padding: 1rem 3rem ;
+  padding: 1rem 3rem;
   color: ${({ theme }) => theme.colors.text};
-
 `;
 
-
-const PageTitle = styled.h1`
-  text-align: center;
-  font-size: 1.9rem;
-  margin: 0;
-`;
-
-const NavLink = styled.a`
+const InternalNavLink = styled(Link)`
   text-decoration: none;
   font-size: 1.3rem;
   font-weight: 600;
@@ -33,6 +22,23 @@ const NavLink = styled.a`
   &:hover {
     color: ${({ theme }) => theme.colors.primary};
   }
+`;
+
+const ExternalNavLink = styled.a`
+  text-decoration: none;
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.text};
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+const PageTitle = styled.h1`
+  text-align: center;
+  font-size: 1.9rem;
+  margin: 0;
 `;
 
 const App: React.FC = () => {
@@ -50,9 +56,11 @@ const App: React.FC = () => {
   return (
     <>
       <Header>
-        <NavLink href="/">Deep Metrics</NavLink>
+        <InternalNavLink to="/">Deep Metrics</InternalNavLink>
         <PageTitle>{pageTitle}</PageTitle>
-        <NavLink href="https://github.com/justyna-przy/DeepMetrics">Github</NavLink>
+        <ExternalNavLink href="https://github.com/justyna-przy/DeepMetrics">
+          Github
+        </ExternalNavLink>
       </Header>
       <Routes>
         <Route path="/" element={<HomePage />} />
